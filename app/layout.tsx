1d058a344f3b40398c8354f1cdf912573e8489a3
ansid <tolyasha5100981@gmail.com>
2026-09-21 23:11:04 +0300

    perf: delay analytics library loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,36 +72,41 @@ export default function RootLayout({
           }}
         />
         {ymId ? (
-          <Script id="yandex-metrika" strategy="afterInteractive">
-            {`
-              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-              ym(${JSON.stringify(ymId)}, "init", {
+          <>
+            <Script id="yandex-metrika" strategy="beforeInteractive">
+              {`
+              window.ym = window.ym || function(){
+                (window.ym.a = window.ym.a || []).push(arguments);
+              };
+              window.ym.l = 1 * new Date();
+              window.ym(${JSON.stringify(ymId)}, "init", {
                 clickmap:true,
                 trackLinks:true,
                 accurateTrackBounce:true,
                 webvisor:true
               });
             `}
-          </Script>
+            </Script>
+            <Script
+              src="https://mc.yandex.ru/metrika/tag.js"
+              strategy="lazyOnload"
+            />
+          </>
         ) : null}
         {gaId ? (
           <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
+            <Script id="google-analytics" strategy="beforeInteractive">
               {`
                 window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${gaId}');
+                window.gtag = window.gtag || function(){window.dataLayer.push(arguments);};
+                window.gtag('js', new Date());
+                window.gtag('config', ${JSON.stringify(gaId)});
               `}
             </Script>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+              strategy="lazyOnload"
+            />
           </>
         ) : null}
       </head>
